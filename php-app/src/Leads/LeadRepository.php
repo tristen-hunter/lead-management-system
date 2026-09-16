f104c40596b6fdf4d6db744219ec0197d06faab4
tristen-hunter <tristenhunter0702@gmail.com>
2026-09-16 17:38:32 +0200

    add lead entity, creation method and db connection

---
+++ php-app/src/Leads/LeadRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Leads;
+
+use App\Config\Database;
+use PDO;
+
+class LeadRepository
+{
+    private PDO $db;
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+    public function create(
+        string $firstName,
+        string $lastName,
+        string $phoneNumber,
+        string $email,
+        ?string $notes = null,
+    ): string {
+        $id = UuidGenerator::generate();
+
+        $sql = "INSERT INTO leads (id, first_name, last_name, phone_number, email, notes)
+            VALUES (:id, :first_name, :last_name, :phone_number, :email, :notes)";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([
+            ':id' => $id,
+            ':first_name' => $firstName,
+            ':last_name' => $lastName,
+            ':phone_number' => $phoneNumber,
+            ':email' => $email,
+            ':notes' => $notes,
+        ]);
+
+        return $id;
+    }
+}
